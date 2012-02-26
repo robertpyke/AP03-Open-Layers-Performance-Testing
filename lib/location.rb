1a0e5789127a2e1e072b443e1778cb049f21104d
Robert Pyke <robert.j.pyke@gmail.com>
2012-02-26 15:01:25 +1000

    Added ID to location. Added a new file to test clustering.

--- lib/location.rb
+++ lib/location.rb
@@ -1,19 +1,21 @@
 class Location
-	attr_reader :lat, :lng
+	@@id = 0
+	attr_reader :id, :lat, :lng
 
 	def initialize lat, lng
+		@id = ( @@id += 1 )
 		@lat = lat
 		@lng = lng
 	end
 
 	# Generates a lat and lng between 0 and 100
 	def Location::generate_random
-		lat =  ( rand(18000) / 100.0 ) - 90
-		lng =  ( rand(36000) / 100.0 ) - 180
+		lat =  ( rand(1800000) / 10000.0 ) - 90
+		lng =  ( rand(3600000) / 10000.0 ) - 180
 		return Location.new(lat, lng)
 	end
 
 	def to_hash
-		return { :lat => @lat, :lng => @lng }
+		return { :id => @id, :lat => @lat, :lng => @lng }
 	end
 end
